3fdd5605e7ddfb936d405c8ea047f35d0615d96b
joao-gallo <contato@kaueguedes.com.br>
2025-01-18 17:27:31 -0300

    modify document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <head>
+        <title>Fluently</title>
+        <meta name="description" content="Uma solução para escolas de idiomas gerir seu ecosistema de forma fácil e eficiente!" />
+      </head>
       <body className="antialiased">
         <ThemeProvider
                     defaultTheme="light"
